629ac58b3f7d61c67b325709d63d544559ad5ef7
valentin <minkowalentin@proton.me>
2024-01-15 22:49:16 +0100

    first commit

---
+++ src/components/RawEditor.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <q-input v-model="text" filled type="textarea" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'RawEditor',
+  setup() {
+    return {
+      text: ref(''),
+    };
+  },
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <RawEditor />
+  </q-page>
+</template>
+
+<script lang="ts">
+import RawEditor from 'components/RawEditor.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: { RawEditor },
+  setup() {
+    return {};
+  },
+});
+</script>
